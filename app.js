d4eb6f43a9e5bf7cc4ed47e2ee66dede250e343c
kgrcbit <64593708+kgrcbit@users.noreply.github.com>
2024-03-17 04:20:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
 const PORT = process.env.PORT || 5000
-const {MONGOURI} = require('./config/keys')
+const MONGOURI='mongodb+srv://sportscbit:wZokJ2Ug0coojB8J@sport-cbit.79n6t5u.mongodb.net/mydb?retryWrites=true&w=majority&appName=sport-cbit'
+//const {MONGOURI} = require('./config/keys')
 
 
 mongoose.connect(MONGOURI,{
